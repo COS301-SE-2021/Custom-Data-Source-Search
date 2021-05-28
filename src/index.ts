ddb9fe9ba1469c89e094072eae5378a4d3349cb3
Joshua Walker <63522140+JoscoW@users.noreply.github.com>
2021-05-29 01:09:54 +0200

    Stop built js files from being pushed

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,29 @@ import express from "express"
 
 const app = express();
 app.listen(3001, () => {
-    console.log('started');
+    console.log('Started Server');
 })
 
 app.get('/', (req,res) =>{
     add(6);
     res.send( "Hello world!");
-    console.log('Added');
+    console.log('Root Accessed');
    })
 
 
-const add = (a: number, b?: number): number => {
+/**
+ *
+ * @param {number} a - The primary value
+ * @param {number} b - The optional secondary value
+ * @returns {number} - returns either the sum of a and b or the sum of a and 2
+ * @author Joshua Walker
+ */
+function add (a: number, b?: number): number {
 
     if(b){
         return a + b;
     }else {
-        return a + 1;
+        return a + 2;
     }
     
 }
-
-console.log("Completed.");
