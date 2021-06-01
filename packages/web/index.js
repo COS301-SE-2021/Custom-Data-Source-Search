5be3f1690d6a4c922681d4eda9f01d15efae5c76
Josua Botha <josua.public@gmail.com>
2021-06-01 19:24:21 +0200

    basic vue component usage scaffold

---
+++ packages/web/index.js
@@ -0,0 +1,15 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+})
