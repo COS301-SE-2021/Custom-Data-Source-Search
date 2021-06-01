2f97e094b619f5fe86424ffa489378fd2eeaae5d
Josua Botha <josua.public@gmail.com>
2021-06-01 19:49:54 +0200

    demo v-bind, v-model, v-on

--- packages/web/index.js
+++ packages/web/index.js
@@ -20,3 +20,30 @@ const AttributeBinding = {
     }
 }
 Vue.createApp(AttributeBinding).mount('#bind-attribute')
+
+
+const EventHandling = {
+    data() {
+        return {
+            message: 'Hello Vue.js!'
+        }
+    },
+    methods: {
+        reverseMessage() {
+            this.message = this.message
+                .split('')
+                .reverse()
+                .join('')
+        }
+    }
+}
+Vue.createApp(EventHandling).mount('#event-handling')
+
+const TwoWayBinding = {
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+}
+Vue.createApp(TwoWayBinding).mount('#two-way-binding')
