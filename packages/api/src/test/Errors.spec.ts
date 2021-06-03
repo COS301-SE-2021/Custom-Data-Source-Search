4a4570e985eed45d2bf30b82db8f807e3e0cae63
Gianni <gianni.volpi.sa@gmail.com>
2021-06-03 08:55:53 +0200

    Add test for FileReadingError

--- packages/api/src/test/Errors.spec.ts
+++ packages/api/src/test/Errors.spec.ts
@@ -10,5 +10,16 @@ describe('FileReadingError object' , () => {
         //then
         expect(fileReadingError.message).toEqual(message);
         expect(fileReadingError.status).toEqual(statusCode);
-    })
+    });
+
+    it('Should construct error object with different properties according to the passed in parameters', () => {
+        //given
+        const message : string = 'other message';
+        const statusCode : number = 500;
+        //when
+        const fileReadingError = new FileReadingError(message,statusCode);
+        //then
+        expect(fileReadingError.message).toEqual(message);
+        expect(fileReadingError.status).toEqual(statusCode);
+    });
 });
