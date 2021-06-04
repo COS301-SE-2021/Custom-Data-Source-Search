3171f1db54c8dd072f7ac9bbaa4454efffa83cbe
Josua Botha <josua.public@gmail.com>
2021-06-04 16:51:38 +0200

    wire up delete datasource

--- packages/web/example.js
+++ packages/web/example.js
@@ -9,6 +9,7 @@ const SearchSystem = {
     },
     methods: {
         search() {
+            this.result = []
             this.queryServer(this.query)
         },
         queryServer(query) {
@@ -33,6 +34,16 @@ const DataSource = {
         }
     },
     methods: {
+        getDataSources() {
+            $.get(
+                "http://localhost:3001/textdatasources/",
+                (data) => {
+                    let number = 0;
+                    this.datasourceList = data
+                    console.log(this.datasourceList)
+                }
+            )
+        },
         addDataSource() {
             $.ajaxSetup(
                 {headers: {'content-type': "application/json"}}
@@ -42,26 +53,31 @@ const DataSource = {
                 url: "http://localhost:3001/textdatasources/",
                 data: JSON.stringify({"fileName" : this.filename, "filePath" : this.datasource}),
                 success: (data) => {
-                    alert(data)
-                    console.log(data.message)
-                    $.get(
-                        "http://localhost:3001/textdatasources/",
-                        (data) => {
-                            console.log(data)
-                            this.datasourceList = data;
-                        }
-                    )
+                    alert(data.message)
+                    console.log(data)
+                    this.getDataSources()
                 },
-                error: (data) => {
-                    alert("Could Not Add Datasource")
+                error: () => {
+                    alert("Could Not Add Datasource...")
                 }
             });
         },
         deleteDataSource(idToDelete) {
-            this.datasourceList = this.datasourceList.filter(item => {
-                return item.data !== idToDelete
-            })
+            if (confirm("Are you sure you want to delete this datasource?")) {
+                $.ajax({
+                    type: "DELETE",
+                    url: "http://localhost:3001/textdatasources/",
+                    data: JSON.stringify({"id": idToDelete}),
+                    success: () => {
+                        this.getDataSources();
+                        alert("Deleted Data Source...");
+                    }
+                })
+            }
         }
+    },
+    beforeMount() {
+        this.getDataSources()
     }
 }
 
