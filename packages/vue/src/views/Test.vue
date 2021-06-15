87e37af2aef138deadd03b10590d72874f285172
Marike <marike@funsail.co.za>
2021-06-15 16:19:26 +0200

    Createde a Tests.vue view to experiment in

---
+++ packages/vue/src/views/Test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="test">
+        <img alt="DataSleuth Logo" src="../assets/dataSmokerSleuth.jpg" style="width: 500px">
+        <HelloWorld msg="Testing out my Vue.js App"/>
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            HelloWorld
+        }
+    }
+</script>
