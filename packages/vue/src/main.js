890bce26e7bae39de6d582d65e47252d5f630540
LaurenRowe99 <u18171215@tuks.co.za>
2021-06-16 20:44:02 +0200

    Added a custom theme

--- packages/vue/src/main.js
+++ packages/vue/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from "element-plus";
-import "element-plus/lib/theme-chalk/index.css";
+import './theme/index.css'
 
 createApp(App).use(router).use(ElementPlus).mount('#app')
