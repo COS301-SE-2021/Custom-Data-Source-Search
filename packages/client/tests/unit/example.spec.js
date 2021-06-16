bb305276417978d32c02c5b8f4956107029718af
Josua Botha <josua.public@gmail.com>
2021-06-16 15:43:03 +0200

    test travis caching

--- packages/client/tests/unit/example.spec.js
+++ packages/client/tests/unit/example.spec.js
@@ -3,7 +3,7 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('renders props.msg when passed in', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
