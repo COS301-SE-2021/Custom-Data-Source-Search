b995fac59bf3de0e66d0231605b7ce4c756c5236
Gianni <gianni.volpi.sa@gmail.com>
2021-06-19 15:30:02 +0200

    Add operations for text datasource store

--- packages/api/src/repositories/TextDataSourceRepository.ts
+++ packages/api/src/repositories/TextDataSourceRepository.ts
@@ -1,4 +1,5 @@
 import {StoredTextDataSource} from "../models/TextDataSource.interface";
+import {randomUUID} from "crypto";
 
 
 class TextDataSourceRepository {
@@ -9,7 +10,24 @@ class TextDataSourceRepository {
         this.textDataSourceArray = [];
     }
 
+    create(dataSource: StoredTextDataSource) {
+        this.textDataSourceArray.push({
+            uuid: randomUUID(),
+            filename: dataSource.filename,
+            path: dataSource.path
+        });
+    }
 
+    read(uuid: string) {
+        let index: number = this.textDataSourceArray.findIndex(x => x.uuid === uuid);
+        if (index !== -1) {
+            return [this.textDataSourceArray[index], null];
+        }
+        return [null, {
+            "code":404,
+            "message":"datasource not found"
+        }]
+    }
 }
 
 const textDataSourceRepository = new TextDataSourceRepository();
