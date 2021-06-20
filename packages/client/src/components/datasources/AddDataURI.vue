cab5826663788f8831bb5aae71938257c7103d56
Josua Botha <josua.public@gmail.com>
2021-06-20 22:58:27 +0200

    Add AddDataURI.vue, input field for new datasource

---
+++ packages/client/src/components/datasources/AddDataURI.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input :placeholder="placeholderPath" v-model="dataSourceName">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddDataURI",
+  props: {
+    placeholderPath: String
+  },
+  data() {
+    return {
+      dataSourceName: ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
