85fb124e88ef5ee46086b4b98dafc8bd0a77ef5a
Josua Botha <josua.public@gmail.com>
2021-06-20 22:45:03 +0200

    Add "add" div into grid

--- packages/client/src/components/datasources/TextDatasource.vue
+++ packages/client/src/components/datasources/TextDatasource.vue
@@ -2,9 +2,12 @@
   <div id="container">
     <div class="grid">
       <div>
-      <span title="File Datasource"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#2ecc71"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"/></svg></span>
+        <span title="File Datasource"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#2ecc71"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"/></svg></span>
       </div>
       <div id="header">Text Files</div>
+      <div>
+        <svg class="add" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><g><rect fill="none" height="24" width="24"/></g><g><g><path d="M19,13h-6v6h-2v-6H5v-2h6V5h2v6h6V13z"/></g></g></svg>
+      </div>
       <div @click="expanded=!expanded" id="expand" >
         <svg class="expand" id="minimise" v-if="expanded" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14l-6-6z"/></svg>
         <svg class="expand" id="expand" v-if="!expanded" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
@@ -49,6 +52,11 @@ export default {
   padding-right: 20px;
 }
 
+.add {
+  float: right;
+  cursor: pointer;
+}
+
 #header {
   font-weight: bold;
   padding-top: 28px;
@@ -64,7 +72,7 @@ export default {
 
 .grid {
   display: grid;
-  grid-template-columns: 1fr 6fr 1fr;
+  grid-template-columns: 1fr 6fr 1fr 1fr;
 }
 
 .grid div {
