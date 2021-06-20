a07f1779e7f754fd577341daeb9280d5e60e6bf5
Joshua Walker <joshwalkerdev99.za@gmail.com>
2021-06-20 16:36:47 +0200

    router methods

--- packages/api/src/routers/WebPageDataSource.router.ts
+++ packages/api/src/routers/WebPageDataSource.router.ts
@@ -1,12 +1,14 @@
 import express, {Request, Response} from "express";
+import {WebPageDataSourceList} from "../models/WebPageDataSource.interface";
+import webPageDataSourceService from "../services/WebPageDataSource.service";
 
 export const webPageDataSourceRouter = express.Router();
 
 webPageDataSourceRouter.get("/", (req: Request, res: Response) => {
     try {
         //const textDataSources: TextDataSourceList = textDataSourceService.getAllTextDataSources();
-        //const webPageDataSources: WebPageDataSourceList = webPageDataSourceService.getAllWebPageDataSources();
-      //  res.status(200).send(textDataSources)
+        const webPageDataSources: WebPageDataSourceList = webPageDataSourceService.getAllWebPageDataSources();
+        res.status(200).send(webPageDataSources)
     } catch (e) {
         res.status(500).send(e.message);
     }
