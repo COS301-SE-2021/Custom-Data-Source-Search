5ba85af9251d7813617b40a380568abe5175abe5
Josua Botha <josua.public@gmail.com>
2021-06-20 13:55:10 +0200

    search on enter, some styling

---
+++ packages/client/src/components/SearchBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <input v-model="query" v-on:keyup.enter="search" placeholder="Sleuth...">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      query: ""
+    }
+  },
+  methods: {
+    search() {
+      alert(this.query)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+div {
+  vertical-align: center;
+}
+
+input {
+  height: 20px;
+  padding-left: 7px;
+  padding-right: 7px;
+  width: 400px;
+  background-color: rgba(211, 211, 211, 0.87);
+  margin-top: -3px;
+  border-radius: 5px;
+  border: none;
+}
+
+input::placeholder {
+  color: black;
+}
+
+.search-button {
+  cursor: pointer;
+  margin-top: 4px;
+  padding-left: 10px;
+}
+
+</style>
--- packages/client/src/views/Search.vue
+++ packages/client/src/views/Search.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchBar/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SearchBar from "../components/SearchBar";
 
 export default {
-  name: 'Home',
+  name: 'Search',
   components: {
-    HelloWorld
+    SearchBar
   }
 }
 </script>
