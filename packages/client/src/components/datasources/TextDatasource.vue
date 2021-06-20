c913b1ff6babe818baaa6ddb02a5e89d256a3f97
Josua Botha <josua.public@gmail.com>
2021-06-20 22:28:26 +0200

    Add DataSourceCard.vue and import it to different datasources

--- packages/client/src/components/datasources/TextDatasource.vue
+++ packages/client/src/components/datasources/TextDatasource.vue
@@ -5,15 +5,24 @@
       <span title="File Datasource"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#2ecc71"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"/></svg></span>
       </div>
       <div id="header">Text Files</div>
-      <div @click="expanded=!expanded" >
+      <div @click="expanded=!expanded" id="expand" >
         <svg class="expand" id="minimise" v-if="expanded" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14l-6-6z"/></svg>
         <svg class="expand" id="expand" v-if="!expanded" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
       </div>
+    </div>
+    <div v-if="expanded" id="text-datasources">
+      <DataSourceCard title="Document"></DataSourceCard>
+      <DataSourceCard title="Something"></DataSourceCard>
+      <DataSourceCard title="Else"></DataSourceCard>
+      <DataSourceCard title="path/name/"></DataSourceCard>
+      <DataSourceCard title="dont/know/what/to"></DataSourceCard>
     </div>
   </div>
 </template>
 
 <script>
+import DataSourceCard from "./DataSourceCard";
+
 export default {
   name: "FileDataSource",
   data() {
@@ -21,6 +30,9 @@ export default {
       expanded: false
 
     }
+  },
+  components: {
+    DataSourceCard
   }
 }
 </script>
@@ -46,6 +58,10 @@ export default {
   float: right;
 }
 
+#expand {
+  cursor: pointer;
+}
+
 .grid {
   display: grid;
   grid-template-columns: 1fr 6fr 1fr;
@@ -57,4 +73,9 @@ export default {
   padding-top: 20px;
   padding-bottom: 20px;
 }
+
+#text-datasources {
+  padding-bottom: 10px;
+}
+
 </style>
