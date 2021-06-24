882b27dcb1a8fc7dd3e47cea42d44a1c643a1d72
LaurenRowe99 <u18171215@tuks.co.za>
2021-06-24 18:56:51 +0200

    Trying to integrate the framework with previous app

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,106 +1,54 @@
 <template>
-  <div id="app">
-    <div className="app-container">
-      <form @submit.prevent="greet">
-        <InputText type="text" v-model="text"/>
-<!--        You can change the outlining of buttons by choosing the colour - in this case we choose our secondary colour-->
-        <Button type="submit" label="Submit"/>
-        <Button icon="pi pi-bell" class="p-button-rounded p-button-warning" />
-        <ToggleButton v-model="checked1" onIcon="pi pi-star" offIcon="pi pi-star-o" />
-        <h3>{{ message }}</h3>
-      </form>
-      <Card>
-        <template #header>
-          Header of a card
-        </template>
-        <template #title>
-          Advanced Card
-        </template>
-        <template #content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
-          quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-        </template>
-        <template #footer>
-          <Button icon="pi pi-check" label="Save" />
-          <Button icon="pi pi-times" label="Cancel" class="p-button-secondary" style="margin-left: .5em" />
-        </template>
-      </Card>
+  <div class="grid-app">
+    <div id="nav">
+      <router-link title="Search" class="icon" to="/"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#26C6DA"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg></router-link>
+      <router-link title="Data Sources" class="icon" to="/datasources"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#26C6DA"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M2 20h20v-4H2v4zm2-3h2v2H4v-2zM2 4v4h20V4H2zm4 3H4V5h2v2zm-4 7h20v-4H2v4zm2-3h2v2H4v-2z"/></svg></router-link>
     </div>
-    <Splitter style="height: 300px">
-      <SplitterPanel>
-        Panel 1
-      </SplitterPanel>
-      <SplitterPanel>
-        <TabView>
-          <TabPanel header="Header I">
-            Content I
-          </TabPanel>
-          <TabPanel header="Header II">
-            Content II
-          </TabPanel>
-          <TabPanel header="Header III">
-            Content III
-          </TabPanel>
-        </TabView>
-      </SplitterPanel>
-    </Splitter>
-    <Toast/>
-
+    <div>
+      <router-view/>
+    </div>
   </div>
 </template>
 
-<script>
-
-export default {
-  data() {
-    return {
-      message: null,
-      text: null,
-      checked1: false
-    }
-  },
-  methods: {
-    greet() {
-      this.$toast.add({severity: 'success', summary: 'Hello ' + this.text});
-      this.message = 'Hello ' + this.text;
-    }
-  },
-}
-</script>
-
-<style scoped lang="scss">
+<style lang="scss">
 html,
 body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
   height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #2c2c2c;
+  color: rgba(255, 255, 255, 0.58);
 }
 
-.app-container {
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
 }
 
-body #app .p-button {
-  margin-left: .2em;
-
+.grid-app {
+  display: grid;
+  grid-template-rows: 1fr 12fr;
 }
 
-///*Easy to change individual elements*/
-button{
-  color: black;
+#nav {
+  padding: 30px;
+  text-align: left;
+
+  a {
+    font-weight: bold;
+    color: rgba(255, 255, 255, 0.58);
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 
-form {
-  margin-top: 2em;
+.icon {
+  padding: 10px;
 }
 
-/*PrimeVue website gives you the names of the classes that can be styled. You can apply regular css styling here. Super helpful!*/
-.p-card{
-  color: white;
-  background-image: linear-gradient(black, grey);
-}
 </style>
