0cc9e7e35d18511ea6f0645b69ec5c6b36523d0b
marike <marike@funsail.co.za>
2021-06-24 11:42:01 +0200

    updated expand buttons

--- packages/client/src/components/datasources/WebpageDatasource.vue
+++ packages/client/src/components/datasources/WebpageDatasource.vue
@@ -10,8 +10,8 @@
         <icon-add class="add" v-else />
       </div>
       <div @click="expanded=!expanded" id="expand">
-        <svg class="expand" id="minimise" v-if="expanded" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14l-6-6z"/></svg>
-        <svg class="expand" id="expand" v-else xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
+        <icon-expand-less class="expand" id="minimise" v-if="expanded" />
+        <icon-expand-more class="expand" v-else />
       </div>
     </div>
     <div v-if="add">
@@ -37,6 +37,8 @@ import axios from "axios";
 import IconWeb from "../icons/IconWeb";
 import IconMin from "../icons/IconMin";
 import IconAdd from "../icons/IconAdd";
+import IconExpandMore from "../icons/IconExpandMore";
+import IconExpandLess from "../icons/IconExpandLess";
 
 export default {
   name: "WebpageDatasource",
@@ -48,6 +50,8 @@ export default {
     }
   },
   components: {
+    IconExpandLess,
+    IconExpandMore,
     IconMin,
     IconAdd,
     IconWeb,
