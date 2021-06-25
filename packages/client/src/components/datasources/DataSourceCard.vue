acf361c95a5a9ec1d5931be0135d903549442479
marike <marike@funsail.co.za>
2021-06-25 16:32:39 +0200

    updated delete icon to have base icon

--- packages/client/src/components/datasources/DataSourceCard.vue
+++ packages/client/src/components/datasources/DataSourceCard.vue
@@ -1,14 +1,16 @@
 <template>
   <div v-if="isNotDeleted">
     <span> {{ title }} </span>
-    <button @click="deleteDataSource(endpoint, id)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#777777"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg></button>
+    <button @click="deleteDataSource(endpoint, id)"><icon-delete/></button>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import IconDelete from "../icons/IconDelete";
 export default {
   name: "DataSourceCard",
+  components: {IconDelete},
   data() {
     return {
       isNotDeleted: true
@@ -37,12 +39,7 @@ export default {
 
 <style scoped>
 
-button {
-  background-color: #212121;
-  border: none;
-  float: right;
-  cursor: pointer;
-}
+
 
 div {
   padding: 15px;
