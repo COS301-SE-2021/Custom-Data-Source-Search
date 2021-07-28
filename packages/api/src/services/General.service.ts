a2798d98aedcfc1eefadce0f36cc379ea4970d18
Gianni <gianni.volpi.sa@gmail.com>
2021-07-28 11:34:39 +0200

    Implement searchAllDataSources in General.service.ts

--- packages/api/src/services/General.service.ts
+++ packages/api/src/services/General.service.ts
@@ -1,6 +1,7 @@
-import textDataSourceService from "./TextDataSource.service";
 import folderDataSourceService from "./FolderDataSource.service";
 import webPageDataSourceService from "./WebPageDataSource.service";
+import axios from "axios";
+import textDataSourceRepository from "../repositories/TextDataSourceRepository";
 
 class GeneralService {
 
@@ -8,10 +9,10 @@ class GeneralService {
         console.log("general Service Created");
     }
 
-    async searchAllDataSources(searchString: string) {
+    async getResults(searchString: string) {
 
         const [folderResults, folderError] = await folderDataSourceService.searchAllFolderDataSources(searchString);
-        const [textResults, textError] = await textDataSourceService.searchAllTextDataSources(searchString);
+        const [textResults, textError] = await this.searchAllDataSources(searchString);
         const [pageResults, pagError] = await webPageDataSourceService.searchAllWebPageDataSources(searchString);
 
         let array: any[] = [];
@@ -39,6 +40,44 @@ class GeneralService {
                 "message": "success",
                 "searchResults": array
             }
+        }
+    }
+
+    async searchAllDataSources(searchString: string) : Promise<[any[], Error]> {
+        try {
+            let response: any  = await axios.get(
+                'http://localhost:8983/solr/files/select?q='
+                + encodeURIComponent(searchString)
+                + '&q.op=OR&hl=true&hl.fl=content&hl.fragsize=200&hl.highlightMultiTerm=false&hl.simple.pre=<em style="color: %2388ffff">&hl.snippets=3'
+            );
+            let docs: any[] = response["data"]["response"]["docs"];
+            let result: any[] = [];
+            for (let [key, value] of Object.entries(response["data"]["highlighting"])) {
+                let currentObject = docs.filter(function (doc) {
+                    return doc.id == key;
+                })[0];
+                // @ts-ignore
+                if (value["content"] != undefined) {
+                    let occurrences: any[] = [];
+                    // @ts-ignore
+                    for (let i = 0; i < value["content"].length; i++) {
+                        // @ts-ignore
+                        occurrences.push({"occurrenceString": value["content"][i]});
+                    }
+                    let [datasource, err] = textDataSourceRepository.getDataSource(key);
+                    // @ts-ignore
+                    if (err) {
+                        // @ts-ignore
+                        result.push({"type": currentObject["datasource_type"], "source": key, "occurrences": occurrences});
+                    } else {
+                        // @ts-ignore
+                        result.push({"type": currentObject["datasource_type"], "source": datasource.path + datasource.filename, "occurrences": occurrences});
+                    }
+                }
+            }
+            return [result, null];
+        } catch (e) {
+            console.error(e)
         }
     }
 }
