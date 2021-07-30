605c9e2fb1afa54f64de1f9eade81be3118e1f8d
Josh <joshwalkerdev99@gmail.com>
2021-07-30 10:37:14 +0200

    move styling to id

--- packages/client/src/views/Search.vue
+++ packages/client/src/views/Search.vue
@@ -13,7 +13,7 @@
             <InputText v-model="query" v-on:keyup.enter="queryServer" placeholder="Sleuth..."/>
 
         </span>
-        <i class="pi pi-info-circle p-text-secondary" style="font-size: 2rem; color: #d69b2c; position: relative; display: inline-block; margin-left: 0.4rem; margin-top : auto; margin-bottom : 0.3rem" v-on:click="showPopup" v-badge.custom-warning="'5'"></i>
+        <i id="expiration-indicator" class="pi pi-info-circle p-text-secondary" v-on:click="showPopup" v-badge.custom-warning="'5'"></i>
 
         </div>
     <SignIn :show="displaySignIn" @display-popup="showPopup"></SignIn>
@@ -119,5 +119,15 @@ input {
 .logo-div {
   text-align: center;
   margin-bottom: 10px;
+}
+
+#expiration-indicator {
+  font-size: 2rem;
+  color: #d69b2c;
+  position: relative;
+  display: inline-block;
+  margin-left: 0.4rem;
+  margin-top : auto;
+  margin-bottom : 0.3rem;
 }
 </style>
