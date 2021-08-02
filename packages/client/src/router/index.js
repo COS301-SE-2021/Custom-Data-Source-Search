54001c1ef2e4ccad2cb15920053e038cc6c00bfe
Josh <joshwalkerdev99@gmail.com>
2021-08-02 11:47:55 +0200

    initial

--- packages/client/src/router/index.js
+++ packages/client/src/router/index.js
@@ -5,6 +5,14 @@ import Register from "../views/Register";
 const routes = [
   {
     path: '/',
+    name: 'Welcome',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Welcome.vue')
+  },
+  {
+    path: '/search',
     name: 'Search',
     component: Search
   },
---
+++ packages/client/src/views/Welcome.vue
@@ -0,0 +1,15 @@
+<template >
+  <h1>Hello</h1>
+
+
+</template>
+
+<script>
+export default {
+  name: "Welcome"
+}
+</script>
+
+<style scoped>
+
+</style>
