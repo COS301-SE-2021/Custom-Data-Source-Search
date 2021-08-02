7a04506edae39594ff325b0ed89f62df68ff7779
Marike <marike@funsail.co.za>
2021-08-02 16:02:47 +0200

    Update style sheet to correct color

--- packages/client/src/views/SettingsPage.vue
+++ packages/client/src/views/SettingsPage.vue
@@ -1,27 +1,52 @@
 <template>
-    <div class="duo-split-grid">
-        <div class="">
+    <div class="settings-box">
+        <div class="info-div">
             <span><b>Personal Information</b></span>
+            <div>
+                //User information
+            </div>
         </div>
-        <div></div>
+        <div class="info-div">
+            <span><b>Backends</b></span>
+            <div>
+                //v-for all the backends information
+            </div>
+        </div>
+        <InputSwitch v-model="checked" />
     </div>
 </template>
 
 <script>
+    import InputSwitch from 'primevue/inputswitch'
     export default {
+        components: {
+            InputSwitch
+        },
+        data () {
+            return {
+                checked: false
+            }
+        },
         name: "SettingsPage"
     }
 </script>
 
 <style scoped>
 
-    .duo-split-grid {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
+    .settings-box {
+        padding: 5px;
     }
 
     b {
 
+    }
+
+    .pi-circle-on {
+      color: #41B3B2;
+    }
+
+    .info-div {
+        padding: 5%;
     }
 
 </style>
