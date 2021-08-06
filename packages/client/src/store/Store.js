7e3d4d481f946a313d25e7adb0fb22c2dca5a650
LaurenRowe99 <u18171215@tuks.co.za>
2021-08-06 14:53:52 +0200

    Added new mutations and getters

--- packages/client/src/store/Store.js
+++ packages/client/src/store/Store.js
@@ -2,16 +2,30 @@ import { createStore } from 'vuex'
 
 const store = createStore({
     state: {
-     signedIn : false,
+        signedIn : false,
+        name: null,
+        email: null
     },
     mutations: {
         setSignedIn(state, payload){
             state.signedIn = payload;
+        },
+        setCurrentUserName(state, payload){
+            state.name = payload;
+        },
+        setCurrentUserEmail(state, payload){
+            state.email = payload;
         }
     },
     getters: {
         getSignedIn(state){
             return state.signedIn;
+        },
+        getName(state){
+            return state.name;
+        },
+        getEmail(state){
+            return state.email;
         }
     }
 
