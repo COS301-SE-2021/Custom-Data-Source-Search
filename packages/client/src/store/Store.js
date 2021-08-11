84d0a9e9b2603cb59f3d8f952cc983f43ccbc84b
Marike <marike@funsail.co.za>
2021-08-11 16:47:57 +0200

    Nav bar won't go away

--- packages/client/src/store/Store.js
+++ packages/client/src/store/Store.js
@@ -3,7 +3,7 @@ import {createStore} from 'vuex'
 const store = createStore({
     state:{
         signedInUserId: 2,
-        signedIn : false,
+        signedIn : true,
         users: [
             {
                 id: 0,
