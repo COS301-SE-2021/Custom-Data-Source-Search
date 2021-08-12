1e75720c05735668de43d1d94fb7a83f348d166a
Josh <joshwalkerdev99@gmail.com>
2021-08-12 22:14:22 +0200

    Added Toolbar

--- packages/client/src/views/BackendManager.vue
+++ packages/client/src/views/BackendManager.vue
@@ -4,6 +4,13 @@
     <h1 class="backend-name">{{backend.name}}</h1>
 
 
+    <Toolbar class="backend-toolbar">
+      <Button label="New" icon="pi pi-plus" class="p-mr-2" />
+      <Button label="Upload" icon="pi pi-upload" class="p-button-success" />
+      <i class="pi pi-bars p-toolbar-separator p-mr-2" />
+      <SplitButton label="Save" icon="pi pi-check" :model="items" class="p-button-warning"></SplitButton>
+    </Toolbar>
+
   </div>
 
 
@@ -55,6 +62,14 @@ export default {
 
   grid-row-start : 1;
   grid-column-start: 2;
+}
+
+.backend-toolbar {
+
+
+  grid-row-start : 3;
+  grid-column-start: 2;
+
 }
 
 </style>
