0c4b5d5b30708d1e3210687238133c1f2c0f9b90
Lauren Rowe <73142577+LaurenRowe99@users.noreply.github.com>
2021-08-14 16:36:06 +0200

    Removed temporary button

--- packages/client/src/views/Datasources.vue
+++ packages/client/src/views/Datasources.vue
@@ -20,9 +20,6 @@
           </span>
             <Button label="Add Data Source" icon="pi pi-plus" class="p-button-text" @click="toggle"
                     style="float: right;"/>
-            <router-link title="Add" to="/addDatasources">
-              <Button label="TEMPORARY!!!" icon="pi pi-plus" class="p-button-text" style="float: right;"/>
-            </router-link>
             <OverlayPanel ref="op" :showCloseIcon="false" :dismissable="true"
                           :breakpoints="{'960px': '75vw', '640px': '100vw'}" :style="{width: '450px'}">
               <div v-if="!clicked && backend===null">
