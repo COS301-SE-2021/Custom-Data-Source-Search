c7594515daf76fa48167dee507a40b68e6943709
Gianni <gianni.volpi.sa@gmail.com>
2021-08-18 22:51:48 +0200

    Update FolderDataSourceRepository to use db

--- packages/api/src/repositories/FolderDataSourceRepository.ts
+++ packages/api/src/repositories/FolderDataSourceRepository.ts
@@ -1,13 +1,9 @@
 import fs from "fs";
 import {FolderDataSource, StoredFolderDataSource} from "../models/FolderDataSource.interface";
 import {randomBytes} from "crypto";
+const db = require("better-sqlite3")('../../data/datasleuth.db');
 
 class FolderDataSourceRepository {
-    folderDataSourceArray: StoredFolderDataSource[];
-
-    constructor() {
-        this.folderDataSourceArray = [];
-    }
 
     addDataSource(dataSource: FolderDataSource): [{ code: number, message: string }, { code: number, message: string }] {
         if (!fs.existsSync(dataSource.path)) {
@@ -16,32 +12,34 @@ class FolderDataSourceRepository {
                 "message": "Directory does not exist"
             }];
         }
-        this.readFile()
-        let index: number = this.folderDataSourceArray.findIndex(x => x.path === dataSource.path);
-        if (index !== -1) {
+        const uuid: string = randomBytes(16).toString("hex")
+        try {
+            db.prepare(
+                'INSERT INTO folder_data (folder_name, path, uuid, tag1, tag2, dot_ignore) VALUES (?, ?, ?, ?, ?, ?);'
+            ).run(
+                FolderDataSourceRepository.getFolderName(dataSource.path),
+                dataSource.path,
+                uuid,
+                dataSource.tag1,
+                dataSource.tag2,
+                "ignore certain files"
+            )
+        } catch (e) {
             return [null, {
                 "code": 400,
                 "message": "Folder datasource already exists"
             }];
         }
-        this.folderDataSourceArray.push({
-            uuid: randomBytes(16).toString("hex"),
-            path: dataSource.path,
-            tag1: dataSource.tag1,
-            tag2: dataSource.tag2
-        });
-        fs.writeFileSync('./store/folderDataStore.json', JSON.stringify(this.folderDataSourceArray));
         return [{
             "code": 200,
-            "message": "Successfully added folder datasource"
+            "message": "Successfully added file datasource"
         }, null];
     }
 
     getDataSource(uuid: string): [StoredFolderDataSource, { "code": number, "message": string }] {
-        this.readFile()
-        let index: number = this.folderDataSourceArray.findIndex(x => x.uuid === uuid);
-        if (index !== -1) {
-            return [this.folderDataSourceArray[index], null];
+        const dataSource = db.prepare("SELECT * FROM folder_data WHERE uuid = ?").get(uuid)
+        if (dataSource !== undefined) {
+            return [FolderDataSourceRepository.castToStoredDataSource(dataSource), null];
         }
         return [null, {
             "code": 404,
@@ -50,33 +48,37 @@ class FolderDataSourceRepository {
     }
 
     getAllDataSources(): [StoredFolderDataSource[], { "code": number, "message": string }] {
-        this.readFile()
-        return [this.folderDataSourceArray, null];
+        const fileDataList = db.prepare("SELECT * FROM file_data;").all()
+        return [fileDataList.map(FolderDataSourceRepository.castToStoredDataSource), null];
     }
 
     deleteDataSource(uuid: string) {
-        this.readFile()
-        let index: number = this.folderDataSourceArray.findIndex(x => x.uuid === uuid);
-        if (index !== -1) {
-            this.folderDataSourceArray.splice(index, 1);
-            fs.writeFileSync('./store/folderDataStore.json', JSON.stringify(this.folderDataSourceArray));
-            return [{
-                "code": 204,
-                "message": "Successfully deleted folder datasource"
-            }, null]
+        try {
+            db.prepare("DELETE FROM folder_data WHERE uuid = ?").run(uuid);
+        } catch (e) {
+            console.error(e)
+            return [null, {
+                "code": 404,
+                "message": "Folder datasource not found"
+            }];
         }
-        return [null, {
-            "code": 404,
-            "message": "Folder datasource not found"
-        }]
+        return [{
+            "code": 204,
+            "message": "Successfully deleted Folder datasource"
+        }, null];
     }
 
-    readFile() {
-        try {
-            this.folderDataSourceArray = JSON.parse(fs.readFileSync('./store/folderDataStore.json', 'utf-8'));
-        } catch (err) {
-            this.folderDataSourceArray = [];
-        }
+    private static getFolderName(path: string) {
+        return path.split("/").pop();
+    }
+
+    private static castToStoredDataSource(dataSource: any) {
+        return {
+            uuid: dataSource.uuid,
+            path: dataSource.path,
+            tag1: dataSource.tag1,
+            tag2: dataSource.tag2
+        };
     }
 }
 
