acadcc8b096dcc86fa90cd254b0e268b25a25881
Gianni <gianni.volpi.sa@gmail.com>
2021-08-19 00:20:30 +0200

    Implement searching of folder datasources

--- packages/api/src/services/General.service.ts
+++ packages/api/src/services/General.service.ts
@@ -104,6 +104,28 @@ class GeneralService {
                                     "match_snippets": webpageOccurrences
                                 });
                                 break;
+                            case "folder":
+                                let folderOccurrences: any[] = [];
+                                let [folderDataSource, folderErr] = webPageDataSourceRepository.getDataSource(key);
+                                if (folderErr) {
+                                    continue;
+                                }
+                                // @ts-ignore
+                                for (let occurrence of value["content"]) {
+                                    folderOccurrences.push({
+                                        "line_number": fileDataSourceService.getSnippetLineNumber(occurrence, currentObject["content"]),
+                                        "snippet": webPageDataSourceService.getSearchSnippet(occurrence)
+                                    });
+                                }
+                                result.push({
+                                    "id": key,
+                                    "type": currentObject["datasource_type"],
+                                    "source": folderDataSource.url,
+                                    "datasource_name": folderDataSource.url,
+                                    "datasource_icon": "<svg height=\"24\" width=\"24\" fill=\"#f1c40f\" viewBox=\"0 0 24 24\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"></path><path d=\"M9.17 6l2 2H20v10H4V6h5.17M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z\"></path></svg>",
+                                    "match_snippets": folderOccurrences
+                                });
+                                break;
                             default:
                                 // code block
                                 console.log("invalid datasource type");
