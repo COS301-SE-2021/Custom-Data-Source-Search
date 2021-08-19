61304e5ee36b7d6bfd3ac9cceaf2b79a7925739b
Marike <marike@funsail.co.za>
2021-08-19 08:02:42 +0200

    Local backend not editable

--- packages/client/src/components/settingsInfo/backendCard.vue
+++ packages/client/src/components/settingsInfo/backendCard.vue
@@ -13,13 +13,13 @@
                     <InputSwitch id="inputswitch" style="float: right; margin-top: 3px"  v-model="local.active"/>
                 </div>
             </div>
-                <div class="expanded-backend-info" v-if="expand">
+                <div class="expanded-backend-info" v-if="expand" :style="localBackendStyle" >
                 <div><em>Email: </em></div>
                 <div> {{ connect.associatedEmail }} </div>
                 <div><em>Link: </em></div>
                 <div> {{connect.link}} </div>
                 <div></div>
-                <div>
+                <div v-if="!localBackendBool" >
                     <Button @click="editBackend" style="float: right" class="p-button p-button-outlined">Edit </Button>
                     <Button @click="showBackendDeleteCheck" style="float: right" class="p-button p-button-outlined">Delete </Button>
                 </div>
@@ -61,6 +61,7 @@
         },
         data () {
             return {
+                localBackendBool: false,
                 displayBackendDeleteCheck: false,
                 tempNameNo: 0,
                 checked: false,
@@ -87,6 +88,11 @@
             ]),
             connectedStyle () {
                 return 'color: ' + this.local.color;
+            },
+            localBackendStyle () {
+                if (this.localBackendBool) {
+                    return "grid-row-gap: 10px"
+                }
             }
         },
         props: {
@@ -223,6 +229,10 @@
                 if (!this.newBackend) {
                     this.tempBackendInfo.name = this.local.name;
                 }
+                if (this.local.name === 'Local') {
+                    this.localBackendBool = true;
+                }
+
                 this.tempBackendInfo.id = this.local.id;
                 this.tempBackendInfo.active = this.local.active;
 
