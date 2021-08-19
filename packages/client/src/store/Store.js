cad89cd7728bd2e7628c26db28943ffc9e274e65
Josua <josua.public@gmail.com>
2021-08-19 09:27:16 +0200

    flip around bool value for prev mutation

--- packages/client/src/store/Store.js
+++ packages/client/src/store/Store.js
@@ -383,7 +383,7 @@ const store = createStore({
             if(secretPair === null) {
                 commit('setBackendLoginStatus', {
                     id: payload.id,
-                    needsLogin: false
+                    needsLogin: true
                 })
                 return;
             }
@@ -398,7 +398,7 @@ const store = createStore({
                 .then((resp) => {
                     commit('setBackendLoginStatus', {
                         id: payload.id,
-                        needsLogin: true
+                        needsLogin: false
                     })
                     commit('setRefreshToken', {
                         id: payload.id,
