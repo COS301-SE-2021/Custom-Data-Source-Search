00d47b31f60f1fb0734cf63c69af0b5217521dd7
Lauren <u18171215@tuks.co.za>
2021-08-25 18:01:05 +0200

    Adjusted styling of tooltip

--- packages/client/src/components/primeComponents/CustomTooltip.vue
+++ packages/client/src/components/primeComponents/CustomTooltip.vue
@@ -34,7 +34,6 @@
     .tooltip-box {
         position: relative;
         display: inline-block;
-      text-align: center;
     }
 
     .tooltip-box:hover .tooltip{
@@ -42,27 +41,38 @@
     }
 
     .tooltip {
-        color: #000000;
-        text-align: center;
-        border-radius: 2px;
-
-        width: fit-content;
-        padding: 1.5vh;
-        top: -50%;
-        left: 120%;
-        /*margin-left: -40px;*/
-
-        opacity: 0;
-        transition: opacity 0.2s;
-
-        position: absolute;
-        z-index: 2;
-
-        background: rgb(121, 179, 177);
+      color: rgba(255, 255, 255, 0.87);
+      border-radius: 2px;
+      width: fit-content;
+      padding: 2vh;
+      top: -50%;
+      left: 120%;
+      opacity: 0;
+      transition: opacity 0.2s;
+      position: absolute;
+      z-index: 2;
+      background: #303030;
+      margin-left: 0.5vw;
+      margin-top: 2vh;
+      box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2), 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);
     }
 
-    span {
+    .tooltip:after{
+      content: "";
+      width: 20px;
+      height: 20px;
+      transform: rotate(-45deg);
+      background: #303030;
+      position: absolute;
+      z-index: -1;
+      top: 10px;
+      left: 0;
+    }
+
+    .text {
+      font-size: 15px;
         height: fit-content;
         padding-top: 2px;
+      float: left;
     }
 </style>
