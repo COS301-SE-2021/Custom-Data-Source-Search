4fbb63798e81a14704742759b198e27058a96ecc
Josh <joshwalkerdev99@gmail.com>
2021-08-28 13:38:57 +0200

    Registration Validation + Tests

--- packages/vault/src/services/Registration.Service.ts
+++ packages/vault/src/services/Registration.Service.ts
@@ -1,5 +1,5 @@
 import {createVerifierAndSalt, SRPParameters, SRPRoutines} from "tssrp6a";
-import {isString} from "util";
+import {isNumber, isString} from "util";
 import {type} from "os";
 import {SRPRegistrationRequest} from "../models/request/Registration.interface";
 import {StandardResponse} from "../models/response/General.interface";
@@ -7,36 +7,37 @@ import vaultRepository from "../repository/Vault.Repository";
 
 class RegistrationService {
 
-
     async register(body: SRPRegistrationRequest): Promise<StandardResponse> {
-
-        if (body.hasOwnProperty("email") &&
-            body.hasOwnProperty("salt") &&
-            body.hasOwnProperty("verifier")
-        ) {
-
+        if(this.detailsAreValid(body)){
             const [data, err] = await vaultRepository.addUser(body.email, body.salt, body.verifier);
 
             if (err) {
                 return {
                     code: 400,
-                    message: "Details Invalid"
+                    message: err
                 }
             } else {
                 return {
                     code: 200,
                     message: "Success"
                 }
-
             }
-            //  const srp6aNimbusRoutines = new SRPRoutines(new SRPParameters());
-            //   const {s: salt, v: verifier} = await createVerifierAndSalt(srp6aNimbusRoutines, "", "");
-    } else {
+        } else {
             return {
                 code: 400,
                 message: "Details Invalid"
             }
         }
+    }
+
+    detailsAreValid(body : SRPRegistrationRequest) : boolean {
+
+        return body.hasOwnProperty("email") &&
+               body.hasOwnProperty("salt") &&
+               body.hasOwnProperty("verifier") &&
+               !isNaN(Number(body.salt)) &&
+               !isNaN(Number(body.verifier));
+
     }
 
 }
---
+++ packages/vault/src/test/Registration.Service.spec.ts
@@ -0,0 +1,58 @@
+import registrationService from "../services/Registration.Service";
+
+const service = registrationService
+
+  describe("Registration Service : Detail Validation",  () => {
+     it("Should Return False if Request Body is null  ", async () => {
+
+         //given
+        const body = {};
+        // when
+        const isValid =  registrationService.detailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(false);
+
+    })
+
+      it("Should Return False if any field is nonexistent ", async () => {
+
+          const body = {
+              email : "example.com",
+              salt : 54545454554545,
+          };
+
+          const isValid =  registrationService.detailsAreValid(body as any);
+
+          expect(isValid).toEqual(false);
+
+      })
+
+      it("Should Return False if salt or verifier are not numbers ", async () => {
+
+          const body = {
+              email : "example.com",
+              salt : 54545454554545,
+              verifier: "fhgfh5445dfhf"
+          };
+
+          const isValid =  registrationService.detailsAreValid(body as any);
+
+          expect(isValid).toEqual(false);
+
+      })
+
+      it("Should Return True if Details are correct ", async () => {
+
+          const body = {
+              email : "example.com",
+              salt : 54545454554545,
+              verifier: 12345677894
+          };
+
+          const isValid =  registrationService.detailsAreValid(body as any);
+
+          expect(isValid).toEqual(true);
+
+      })
+
+})
