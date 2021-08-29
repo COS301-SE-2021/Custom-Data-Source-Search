f6ccf3261bd91ddf2b7277b44bf1759e1f255e2c
Lauren <u18171215@tuks.co.za>
2021-08-29 21:08:27 +0200

    Fixed errors created during merge from dev

--- packages/client/src/components/datasources/DatasourcesTable.vue
+++ packages/client/src/components/datasources/DatasourcesTable.vue
@@ -258,250 +258,210 @@
     import AddWebpageDatasource from "./webpage/AddWebpageDatasource";
 
     export default {
-        name: "DatasourcesTable",
+      name: "DatasourcesTable",
 
-        components: {
-            AddFileDatasource,
-            AddFolderDatasource,
-            AddWebpageDatasource
-        },
-
-<<<<<<< Updated upstream
-        data() {
-            return {
-                message: "No sources have been selected.",
-                type: null,
-                clicked: false,
-                sources: null,
-                loading: false,
-                backend: null,
-                selectedSources: null,
-                filters: {
-                    'global': {value: null, matchMode: FilterMatchMode.CONTAINS},
-                    'location': {value: null, matchMode: FilterMatchMode.CONTAINS},
-                    'backend': {value: null, matchMode: FilterMatchMode.IN},
-                    'type': {value: null, matchMode: FilterMatchMode.IN},
-                    'tag1': {value: null, matchMode: FilterMatchMode.CONTAINS},
-                    'tag2': {value: null, matchMode: FilterMatchMode.CONTAINS},
-                },
-                types: [
-                    'file', 'folder', 'webpage'
-                ],
-                backends: [],
-            }
-        },
-=======
-  data() {
-    return {
-      message: "No sources have been selected.",
-      type: null,
-      clicked: false,
-      sources: null,
-      loading: false,
-      backend: null,
-      selectedSources: null,
-      filters: {
-        'global': {value: null, matchMode: FilterMatchMode.CONTAINS},
-        'location': {value: null, matchMode: FilterMatchMode.CONTAINS},
-        'backend': {value: null, matchMode: FilterMatchMode.IN},
-        'type': {value: null, matchMode: FilterMatchMode.IN},
-        'tag1': {value: null, matchMode: FilterMatchMode.CONTAINS},
-        'tag2': {value: null, matchMode: FilterMatchMode.CONTAINS},
+      components: {
+        AddFileDatasource,
+        AddFolderDatasource,
+        AddWebpageDatasource
       },
-      types: [
-        'file', 'folder', 'webpage'
-      ],
-      backends: [],
-      items: [
-        {
-          label: 'Choose an action',
-          items: [{
-            label: 'Delete Selected',
-            icon: 'pi pi-trash',
-            command: () => {
-              this.deleteSource();
-            }
-          },
-            {
-              label: 'Edit',
-              icon: 'pi pi-times',
-              command: () => {
-                // this.editSource();
-              }
-            }
-          ]}
-      ]
-    }
-  },
->>>>>>> Stashed changes
 
-        beforeMount() {
-          if (this.$store.getters.getNewAppStatus) {
-            this.$router.push('/');
-          }
-          this.backends = this.$store.getters.getUserBackendNames;
-          console.log(this.$store.getters.getUserBackends(this.$store.getters.getSignedInUserId));
-          this.updateSources();
+      data() {
+        return {
+          message: "No sources have been selected.",
+          type: null,
+          clicked: false,
+          sources: null,
+          loading: false,
+          backend: null,
+          selectedSources: null,
+          filters: {
+            'global': {value: null, matchMode: FilterMatchMode.CONTAINS},
+            'location': {value: null, matchMode: FilterMatchMode.CONTAINS},
+            'backend': {value: null, matchMode: FilterMatchMode.IN},
+            'type': {value: null, matchMode: FilterMatchMode.IN},
+            'tag1': {value: null, matchMode: FilterMatchMode.CONTAINS},
+            'tag2': {value: null, matchMode: FilterMatchMode.CONTAINS},
+          },
+          types: [
+            'file', 'folder', 'webpage'
+          ],
+          backends: [],
+          items: [
+            {
+              label: 'Choose an action',
+              items: [{
+                label: 'Delete Selected',
+                icon: 'pi pi-trash',
+                command: () => {
+                  this.deleteSource();
+                }
+              },
+                {
+                  label: 'Edit',
+                  icon: 'pi pi-times',
+                  command: () => {
+                    // this.editSource();
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      },
+
+      beforeMount() {
+        if (this.$store.getters.getNewAppStatus) {
+          this.$router.push('/');
+        }
+        this.backends = this.$store.getters.getUserBackendNames;
+        console.log(this.$store.getters.getUserBackends(this.$store.getters.getSignedInUserId));
+        this.updateSources();
+      },
+
+      productService: null,
+
+      methods: {
+        toggleMenu(event) {
+          this.$refs.menu.toggle(event);
         },
 
-<<<<<<< Updated upstream
-        productService: null,
-=======
-  productService: null,
-  
-  methods: {
-    toggleMenu(event) {
-      this.$refs.menu.toggle(event);
-    },
+        /**
+         * Toggles the visibility of the overlay panel
+         * @param event
+         */
+        toggle(event) {
+          this.$refs.op.toggle(event);
+          this.clicked = false;
+          this.backend = null;
+        },
 
-    /**
-     * Toggles the visibility of the overlay panel
-     * @param event
-     */
-    toggle(event) {
-      this.$refs.op.toggle(event);
-      this.clicked = false;
-      this.backend = null;
-    },
->>>>>>> Stashed changes
-
-        methods: {
-            /**
-             * Toggles the visibility of the overlay panel
-             * @param event
-             */
-            toggle(event) {
-                this.$refs.op.toggle(event);
-                this.clicked = false;
-                this.backend = null;
-            },
-
-            updateSources() {
-                this.loading = true;
-                this.sources = [];
-                for (let backend of this.$store.getters.getUserBackends(this.$store.getters.getSignedInUserId)) {
-                    const url = `http://${backend.connect.link}/general/datasources`;
-                    const headers = {
-                        "Authorization": "Bearer " + backend.connect.keys.jwtToken
-                    };
-                    axios
-                        .get(url, {headers})
-                        .then((resp) => {
-                            this.handleSuccess(resp.data.data, backend.connect.link, backend.local.id, backend.local.name);
-                        })
-                        .catch(async () => {
-                            await this.$store.dispatch("refreshJWTToken", {id: backend.local.id});
-                            const headers = {
-                                "Authorization": "Bearer " + this.$store.getters.getBackendJWTToken(backend.local.id)
-                            };
-                            await axios
-                                .get(url, {headers})
-                                .then((resp) => {
-                                    this.handleSuccess(resp.data.data, backend.connect.link, backend.local.id, backend.local.name);
-                                })
-                                .catch((e) => {
-                                    console.error(e);
-                                })
-                        })
-                }
-            },
-
-            /**
-             * Method called on success of the axios call made in updateSources.
-             *
-             * @param results - data returned by axios call
-             * @param link - backend link to which the axios call is made
-             * @param id - id belonging to the backend to which the axios call is made
-             * @param name - name belonging to the backend to which the axios call is made
-             */
-            handleSuccess(results, link, id, name) {
-                for (let r of results) {
-                    r.link = link;
-                    r.backendId = id;
-                    r.backend = name;
-                }
-                this.sources = this.sources.concat(results);
-
-                if (this.sources.length === 0) {
-                    this.$toast.add({
-                        severity: 'warn',
-                        summary: 'No sources',
-                        detail: "Try adding data sources",
-                        life: 3000
-                    });
-                }
-                this.loading = false;
-            },
-
-            /**
-             * Queries the store to check the admin status associated with the user for a specific backend.
-             *
-             * @param source - name of the backend to be queried
-             * @returns {boolean|*} - returns a boolean indicating whether a user has admin privileges (true) or not (false)
-             */
-            datasourceAdminStatus(source) {
-                if (source === "Local") {
-                    return true;
-                } else {
-                    return this.$store.getters.getBackendAdminStatus(source);
-                }
-            },
-
-            deleteSource() {
-                if (this.selectedSources === null) {
-                    this.$toast.add({
-                        severity: 'info',
-                        summary: 'No Sources Selected',
-                        detail: 'Please select sources to delete',
-                        life: 3000
-                    });
-                    return;
-                } else if (this.selectedSources.length === 1) {
-                    this.message = "Are you sure you want to delete this data source?";
-                } else if (this.selectedSources.length > 1) {
-                    this.message = "Are you sure you want to delete these data sources?";
-                }
-                this.$confirm.require({
-                    message: this.message,
-                    header: 'Confirmation',
-                    icon: 'pi pi-exclamation-triangle',
-                    acceptClass: "p-button-danger",
-                    rejectClass: "p-button-text p-button-plain",
-                    accept: () => {
-                        let source;
-                        for (source in this.selectedSources) {
-                            const url = `http://${this.selectedSources[source].link}/general/datasources`;
-                            console.log(url);
-                            axios
-                                .delete(url, {
-                                    "data": {
-                                        "type": this.selectedSources[source].type,
-                                        "id": this.selectedSources[source].id
-                                    }
-                                })
-                                .then(() => {
-                                    this.$toast.add({
-                                        severity: 'success',
-                                        summary: 'Deleted',
-                                        detail: "Source deleted",
-                                        life: 3000
-                                    });
-                                    this.updateSources();
-                                })
-                                .catch(() => {
-                                    this.$toast.add({
-                                        severity: 'error',
-                                        summary: 'Error',
-                                        detail: "Could not delete source",
-                                        life: 3000
-                                    });
-                                })
-                        }
-                        this.selectedSources = null;
-                    }
+        updateSources() {
+          this.loading = true;
+          this.sources = [];
+          for (let backend of this.$store.getters.getUserBackends(this.$store.getters.getSignedInUserId)) {
+            const url = `http://${backend.connect.link}/general/datasources`;
+            const headers = {
+              "Authorization": "Bearer " + backend.connect.keys.jwtToken
+            };
+            axios
+                .get(url, {headers})
+                .then((resp) => {
+                  this.handleSuccess(resp.data.data, backend.connect.link, backend.local.id, backend.local.name);
                 })
+                .catch(async () => {
+                  await this.$store.dispatch("refreshJWTToken", {id: backend.local.id});
+                  const headers = {
+                    "Authorization": "Bearer " + this.$store.getters.getBackendJWTToken(backend.local.id)
+                  };
+                  await axios
+                      .get(url, {headers})
+                      .then((resp) => {
+                        this.handleSuccess(resp.data.data, backend.connect.link, backend.local.id, backend.local.name);
+                      })
+                      .catch((e) => {
+                        console.error(e);
+                      })
+                })
+          }
+        },
+
+        /**
+         * Method called on success of the axios call made in updateSources.
+         *
+         * @param results - data returned by axios call
+         * @param link - backend link to which the axios call is made
+         * @param id - id belonging to the backend to which the axios call is made
+         * @param name - name belonging to the backend to which the axios call is made
+         */
+        handleSuccess(results, link, id, name) {
+          for (let r of results) {
+            r.link = link;
+            r.backendId = id;
+            r.backend = name;
+          }
+          this.sources = this.sources.concat(results);
+
+          if (this.sources.length === 0) {
+            this.$toast.add({
+              severity: 'warn',
+              summary: 'No sources',
+              detail: "Try adding data sources",
+              life: 3000
+            });
+          }
+          this.loading = false;
+        },
+
+        /**
+         * Queries the store to check the admin status associated with the user for a specific backend.
+         *
+         * @param source - name of the backend to be queried
+         * @returns {boolean|*} - returns a boolean indicating whether a user has admin privileges (true) or not (false)
+         */
+        datasourceAdminStatus(source) {
+          if (source === "Local") {
+            return true;
+          } else {
+            return this.$store.getters.getBackendAdminStatus(source);
+          }
+        },
+
+        deleteSource() {
+          if (this.selectedSources === null) {
+            this.$toast.add({
+              severity: 'info',
+              summary: 'No Sources Selected',
+              detail: 'Please select sources to delete',
+              life: 3000
+            });
+            return;
+          } else if (this.selectedSources.length === 1) {
+            this.message = "Are you sure you want to delete this data source?";
+          } else if (this.selectedSources.length > 1) {
+            this.message = "Are you sure you want to delete these data sources?";
+          }
+          this.$confirm.require({
+            message: this.message,
+            header: 'Confirmation',
+            icon: 'pi pi-exclamation-triangle',
+            acceptClass: "p-button-danger",
+            rejectClass: "p-button-text p-button-plain",
+            accept: () => {
+              let source;
+              for (source in this.selectedSources) {
+                const url = `http://${this.selectedSources[source].link}/general/datasources`;
+                console.log(url);
+                axios
+                    .delete(url, {
+                      "data": {
+                        "type": this.selectedSources[source].type,
+                        "id": this.selectedSources[source].id
+                      }
+                    })
+                    .then(() => {
+                      this.$toast.add({
+                        severity: 'success',
+                        summary: 'Deleted',
+                        detail: "Source deleted",
+                        life: 3000
+                      });
+                      this.updateSources();
+                    })
+                    .catch(() => {
+                      this.$toast.add({
+                        severity: 'error',
+                        summary: 'Error',
+                        detail: "Could not delete source",
+                        life: 3000
+                      });
+                    })
+              }
+              this.selectedSources = null;
             }
+          })
         }
+      }
     }
 </script>
 
