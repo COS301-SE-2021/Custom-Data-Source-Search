e94010d06c11d7a7428eafc56292a27fee2412b8
Josua <josua.public@gmail.com>
2021-09-08 18:05:57 +0200

    use sh ./ to call other shell scripts

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 if [ "$SITE_FLAVOUR" = "api" ];
 then
-  deploy.api.sh
+  sh ./deploy.api.sh
 elif [ "$SITE_FLAVOUR" = "client" ];
 then
-  deploy.client.sh
+  sh ./deploy.client.sh
 else
   echo "Something went wrong with your site flavour ($SITE_FLAVOUR)! Should be either api or client."
 fi
