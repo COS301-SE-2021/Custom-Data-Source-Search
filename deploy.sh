a19db823d75d9b719135855115c50a0de22d844e
Josua <josua.public@gmail.com>
2021-09-08 17:37:51 +0200

    first set variable before using ${} notation

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ elif "%SITE_FLAVOUR%" == "client";
 then
   deploy.client.sh
 else
-echo "Something went wrong with your site flavour ${%SITE_FLAVOUR%} Should be either api or client."
+  a =  %SITE_FLAVOUR%
+  echo "Something went wrong with your site flavour (${a}). Should be either api or client."
 fi
