ebec4914c254faf68165d620de8952e129974241
Josua <josua.public@gmail.com>
2021-09-08 17:39:42 +0200

    remove spaces around variable assignement

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ elif "%SITE_FLAVOUR%" == "client";
 then
   deploy.client.sh
 else
-  a =  %SITE_FLAVOUR%
+  a=%SITE_FLAVOUR%
   echo "Something went wrong with your site flavour (${a}). Should be either api or client."
 fi
