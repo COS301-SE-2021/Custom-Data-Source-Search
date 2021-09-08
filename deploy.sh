ef95a6451cb46c31f5745d8ab918e66987d54628
Josua <josua.public@gmail.com>
2021-09-08 18:09:39 +0200

    add #!/bin/bash to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 if [ "$SITE_FLAVOUR" = "api" ];
 then
-  sh ./deploy.api.sh
+  ./deploy.api.sh
 elif [ "$SITE_FLAVOUR" = "client" ];
 then
-  sh ./deploy.client.sh
+  ./deploy.client.sh
 else
   echo "Something went wrong with your site flavour ($SITE_FLAVOUR)! Should be either api or client."
 fi
