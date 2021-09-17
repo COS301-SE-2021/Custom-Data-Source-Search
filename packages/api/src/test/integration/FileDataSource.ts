e8cd517e77226ad3983af89220052d3e340166d2
Gianni <gianni.volpi.sa@gmail.com>
2021-09-17 16:19:24 +0200

    Start integration testing

--- packages/api/src/test/integration/FileDataSource.ts
+++ packages/api/src/test/integration/FileDataSource.ts
@@ -1,17 +1,28 @@
-
-
-
-// describe("File datasource", () => {
-//     it("Get all file datasources endpoint should return status code of 200 and empty array when db is empty",
-//         async () => {
-//         const request = supertest(server)
-//         jest.spyOn(fileDataSourceRepository, "getAllDataSources").mockImplementation(() => {
-//             return [[], null];
-//         });
-//         const response = await request
-//             .get('/filedatasources')
-//             .set('Accept', 'application/json');
-//         expect(response.status).toBe(200);
-//         expect(response.body).toEqual([]);
-//     });
-// });
+import express from "express";
+import {fileDataSourceRouter} from "../../routers/FileDataSource.router";
+const request = require("supertest"); // supertest is a framework that allows to easily test web apis
+const app = express(); //an instance of an express app, a 'fake' express app
+app.use("/filedatasources", fileDataSourceRouter); //routes
+describe("Testing file datasource routes", () => {
+    it("GET /states - success", async () => {
+        const { body } = await request(app).get("/filedatasources"); //uses the request function that calls on express app instance
+        console.log(body);
+        expect(body).toEqual([
+            {
+                state: "NJ",
+                capital: "Trenton",
+                governor: "Phil Murphy",
+            },
+            {
+                state: "CT",
+                capital: "Hartford",
+                governor: "Ned Lamont",
+            },
+            {
+                state: "NY",
+                capital: "Albany",
+                governor: "Andrew Cuomo",
+            },
+        ]);
+    });
+});
