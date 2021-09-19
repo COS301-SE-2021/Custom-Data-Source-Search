8ed6a6c39eafc3fd837d5a1437213771dfaf5026
Lauren <u18171215@tuks.co.za>
2021-09-19 23:22:41 +0200

    Fixed spacing on admin backend card

--- packages/client/src/components/admin/AdminBackendCard.vue
+++ packages/client/src/components/admin/AdminBackendCard.vue
@@ -80,7 +80,6 @@
   border-width: 1px;
   border-color: rgba(158,41,34,0);
   padding-top: 0.5em;
-  padding-left: 0.6em;
   background-color: #2c2c2c;
   box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
 
