3f5327ff66c09023df076bff2c275d1c7348c6c8
Josh <joshwalkerdev99@gmail.com>
2021-09-20 00:00:38 +0200

    Final tests

--- packages/vault/src/test/Authentication.Service.spec.ts
+++ packages/vault/src/test/Authentication.Service.spec.ts
@@ -5,22 +5,9 @@ const service = authenticationService
 
 /*
 
-export interface SRPPullRequest {
-    email : string,
-    A : bigint,
-    verificationMessage1 : bigint
-}
 
-export interface SRPPushRequest {
-    email : string,
-    A : bigint,
-    verificationMessage1 : bigint
-    user_data: string,
-    fingerprint: string,
-    user_iv: string,
-    user_authtag: string,
-    user_salt: string,
-}
+
+
 
 export interface SRPDeleteRequest {
     email : string,
@@ -157,3 +144,167 @@ describe("Authentication Service: Compare Details Validation", () => {
         expect(isValid).toEqual(true);
     })
 })
+
+describe("Authentication Service: Pull Details Validation", () => {
+    it("Should Return False if Request Body is null", async () => {
+        //given
+        const body = {};
+        // when
+        const isValid = service.pullDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(false);
+    })
+
+    it("Should Return False if one field is nonexistent", async () => {
+        //given
+        const body = {
+            email: "name@example.com",
+            verificationMessage1 : 111111111111111,
+        };
+        //when
+        const isValid = service.pullDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(false);
+    })
+
+    it("Should Return False if fingerprint or vM1 is not a number ", async () => {
+        //given
+        const body = {
+            email: "name@example.com",
+            fingerprint: 1111111111111111111111,
+            verificationMessage1 : 1111111111111111111,
+        };
+        //when
+        const isValid = service.pullDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(false);
+    })
+
+    it("Should Return True if Details are correct", async () => {
+        //given
+        const body = {
+            email: "name@example.com",
+            A: 11111111111111111111111111,
+            verificationMessage1 : 11111111111111111111111
+        }
+        //when
+        const isValid = service.pullDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(true);
+    })
+})
+
+describe("Authentication Service: Push Details Validation", () => {
+    it("Should Return False if Request Body is null", async () => {
+        //given
+        const body = {};
+        // when
+        const isValid = service.pushDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(false);
+    })
+
+    it("Should Return False if one field is nonexistent", async () => {
+        //given
+        const body = {
+            email: "name@example.com",
+            verificationMessage1 : 111111111111111,
+            user_data: randomBytes(16).toString('hex'),
+            fingerprint: randomBytes(16).toString('hex'),
+            user_iv: randomBytes(16).toString('hex'),
+            user_authtag: randomBytes(16).toString('hex'),
+            user_salt: randomBytes(16).toString('hex'),
+        };
+        //when
+        const isValid = service.pushDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(false);
+    })
+
+    it("Should Return False if a BigInt is not a number ", async () => {
+        //given
+        const body = {
+            email: "name@example.com",
+            A : 11111111111,
+            verificationMessage1 : randomBytes(16).toString('hex'),
+            user_data: randomBytes(16).toString('hex'),
+            fingerprint: randomBytes(16).toString('hex'),
+            user_iv: randomBytes(16).toString('hex'),
+            user_authtag: randomBytes(16).toString('hex'),
+            user_salt: randomBytes(16).toString('hex'),
+        };
+        //when
+        const isValid = service.pushDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(false);
+    })
+
+    it("Should Return True if Details are correct", async () => {
+        //given
+        const body = {
+            email: "name@example.com",
+            A : 11111111111111,
+            verificationMessage1 : 1111111111111111111111111,
+            user_data: randomBytes(16).toString('hex'),
+            fingerprint: randomBytes(16).toString('hex'),
+            user_iv: randomBytes(16).toString('hex'),
+            user_authtag: randomBytes(16).toString('hex'),
+            user_salt: randomBytes(16).toString('hex'),
+        }
+        //when
+        const isValid = service.pushDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(true);
+    })
+})
+
+describe("Authentication Service: Delete Details Validation", () => {
+    it("Should Return False if Request Body is null", async () => {
+        //given
+        const body = {};
+        // when
+        const isValid = service.deleteDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(false);
+    })
+
+    it("Should Return False if one field is nonexistent", async () => {
+        //given
+        const body = {
+            email: "name@example.com",
+            verificationMessage1 : 111111111111111,
+        };
+        //when
+        const isValid = service.deleteDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(false);
+    })
+
+    it("Should Return False if fingerprint or vM1 is not a number ", async () => {
+        //given
+        const body = {
+            email: "name@example.com",
+            fingerprint: 1111111111111111111111,
+            verificationMessage1 : 1111111111111111111,
+        };
+        //when
+        const isValid = service.deleteDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(false);
+    })
+
+    it("Should Return True if Details are correct", async () => {
+        //given
+        const body = {
+            email: "name@example.com",
+            A: 11111111111111111111111111,
+            verificationMessage1 : 11111111111111111111111
+        }
+        //when
+        const isValid = service.deleteDetailsAreValid(body as any);
+        //then
+        expect(isValid).toEqual(true);
+    })
+})
+
+
