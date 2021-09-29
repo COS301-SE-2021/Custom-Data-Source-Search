6cfd0e46c6e1dc5e671d476ebd9c702ec4240da3
Lauren <u18171215@tuks.co.za>
2021-09-29 17:28:53 +0200

    Infinite data loading bug fixed

--- packages/client/src/components/datasources/DatasourcesTable.vue
+++ packages/client/src/components/datasources/DatasourcesTable.vue
@@ -346,9 +346,6 @@
 
     mounted(){
       this.$refs.Global.$el.focus();
-      // this.$root.$on('globalUpdate', () => {
-      //       this.updateSources();
-      //     });
     },
 
     after(){
@@ -380,30 +377,33 @@
           this.loading = true;
           this.sources = [];
           for (let backend of this.$store.getters.getUserBackends(this.$store.getters.getSignedInUserId)) {
-            const url = `http://${backend.connect.link}/general/datasources`;
-            const headers = {
-              "Authorization": "Bearer " + backend.connect.keys.jwtToken
-            };
-            axios
-                .get(url, {headers})
-                .then((resp) => {
-                  this.handleSuccess(resp.data.data, backend.connect.link, backend.local.id, backend.local.name);
-                })
-                .catch(async () => {
-                  await this.$store.dispatch("refreshJWTToken", {id: backend.local.id});
-                  const headers = {
-                    "Authorization": "Bearer " + this.$store.getters.getBackendJWTToken(backend.local.id)
-                  };
-                  await axios
-                      .get(url, {headers})
-                      .then((resp) => {
-                        this.handleSuccess(resp.data.data, backend.connect.link, backend.local.id, backend.local.name);
-                      })
-                      .catch((e) => {
-                        console.error(e);
-                      })
-                })
+            if(backend.local.active){
+              const url = `http://${backend.connect.link}/general/datasources`;
+              const headers = {
+                "Authorization": "Bearer " + backend.connect.keys.jwtToken
+              };
+              axios
+                  .get(url, {headers})
+                  .then((resp) => {
+                    this.handleSuccess(resp.data.data, backend.connect.link, backend.local.id, backend.local.name);
+                  })
+                  .catch(async () => {
+                    await this.$store.dispatch("refreshJWTToken", {id: backend.local.id});
+                    const headers = {
+                      "Authorization": "Bearer " + this.$store.getters.getBackendJWTToken(backend.local.id)
+                    };
+                    await axios
+                        .get(url, {headers})
+                        .then((resp) => {
+                          this.handleSuccess(resp.data.data, backend.connect.link, backend.local.id, backend.local.name);
+                        })
+                        .catch((e) => {
+                          console.error(e);
+                        })
+                  })
+            }
           }
+          this.loading = false;
         },
 
         /**
