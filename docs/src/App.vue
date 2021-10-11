db561815b78850513e16c85bac2ec82e5e0f38e4
Lauren <u18171215@tuks.co.za>
2021-10-11 15:51:22 +0200

    Created Vue website structure

---
+++ docs/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App.vue",
+
+  data(){
+    return{
+      message: "Hello World",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#app{
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
+}
+</style>
